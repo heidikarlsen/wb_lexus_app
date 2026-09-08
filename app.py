f8d84e99936e309c41e08fa60584485a8db3cd22
Karlsen <heidi.karlsen@bi.no>
2026-09-08 12:42:32 +0200

    adjusting terms for the relevance screening function

--- app.py
+++ app.py
@@ -304,6 +304,12 @@ def calculate_case_relevance(case_df):
         "retaliation": re.compile(
             r"\bretaliation\b",
             re.IGNORECASE
+        ),
+
+        # Allows multiple spaces / line breaks
+        "unfair dismissal": re.compile(
+            r"\bunfair\s+dismissal\b",
+            re.IGNORECASE
         )
     }
 
